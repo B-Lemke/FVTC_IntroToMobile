8bb05f0d520a59a0cf164138af98d35c6a121332
Student <blemke4@gmail.com>
2018-09-18 19:31:07 -0500

    Adding hello world application to version control.

---
+++ HelloWorld/HelloWorld/HelloWorldPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Xamarin.Forms;
+using System;
+
+namespace HelloWorld
+{
+    public partial class HelloWorldPage : ContentPage
+    {
+        //Global Vars
+        bool switched = false;
+
+        public HelloWorldPage()
+        {
+            InitializeComponent();
+        }
+
+        public void HandleButtonClick(object sender, System.EventArgs e)
+        {
+            if (switched == false)
+            {
+                ChangingLabel.Text = "💯💯👌😂👌💯💯";
+                ButtonChanging.Text = "Oh god, please stop the emojis";
+            }
+            else
+            {
+                ChangingLabel.Text = "No more emojis, woooooo";
+                ButtonChanging.Text = "Show trashy emojis";
+            }
+            //Change switched variable
+            switched = !switched;
+        }
+    }
+}

a86088f29eb69ef52a825c04d3a1e8ee202232fb
Student <blemke4@gmail.com>
2018-09-19 21:55:48 -0500

    Creating mobile application for addition with emoji binary representation

---
+++ Calculator/Calculator/CalculatorPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Calculator
+{
+    public partial class CalculatorPage : ContentPage
+    {
+        public CalculatorPage()
+        {
+            InitializeComponent();
+        }
+
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            int numberOne, numberTwo;
+
+            if (int.TryParse(txtNumberOne.Text, out numberOne))
+            {
+                if (int.TryParse(txtNumberTwo.Text, out numberTwo))
+                {
+                    //Both numbers parse successfully. 
+                    int sum = numberOne + numberTwo;
+
+
+                    string binaryNumber = Convert.ToString(sum, 2);
+                    string output = string.Empty;
+
+                    foreach (char bit in binaryNumber)
+                    {
+                        string addChar;
+
+                        if (bit == '1')
+                        {
+                            addChar = "😂";
+                        }
+                        else
+                        {
+                            addChar = "💦";
+                        }
+
+                        output += addChar;
+
+                    }
+
+                    lblDisplay.Text = output;
+                    lblDisplayNum.Text = "("+sum.ToString()+")";
+
+
+                }
+                else
+                {
+                    //bad second number
+                    lblDisplay.Text = "Second number must be numeric.";
+                }
+            }
+            else
+            {
+                //Bad first number
+                lblDisplay.Text = "First number must be numeric.";
+
+            }
+        }
+
+    }
+}
